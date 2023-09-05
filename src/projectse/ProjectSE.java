5daafc639e5127b07af4abc765320bf2f564a122
lamaalamri <lsaeedalamri0001@stu.kau.edu.sa>
2023-09-05 14:28:54 +0300

    first commit!

--- src/projectse/ProjectSE.java
+++ src/projectse/ProjectSE.java
@@ -15,6 +15,9 @@ public class ProjectSE {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+       System.out.print("Lama Abdulaziz Alamri-2107528");
+              
         // TODO code application logic here
     }
     
